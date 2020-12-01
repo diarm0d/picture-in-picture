ce1a59d1443c988f239dafa39256ec9f611bd36a
diarm0d <diarmuid.odonovan@protonmail.com>
2020-12-01 17:14:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement = document.getElementById('video');
+const startButton = document.getElementById('start-button');
+
+//Prompt to select media steam, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata= () => {
+        videoElement.onplay();
+            }
+    } catch (error) {
+            //catch error here
+    }
+}
+
+startButton.addEventListener('click', async () => {
+    console.log('hello')
+    // Disable button
+    startButton.disabled = true;
+    // start in picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset buttion
+    startButton.disabled = false;
+});
+
+// on load
+selectMediaStream();
